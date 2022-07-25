7f45fcd706813ddc5e673faead0503e44887d8a7
Syuugo <pub@s1204.me>
2022-07-25 18:23:15 +0900

    v1

--- app/src/main/java/jp/co/benesse/touch/setuplogin/MainActivity.java
+++ app/src/main/java/jp/co/benesse/touch/setuplogin/MainActivity.java
@@ -1,31 +1,29 @@
 package jp.co.benesse.touch.setuplogin;
 
-import android.app.*;
-import android.os.*;
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.provider.Settings;
 import android.provider.Settings.System;
 import android.content.Intent;
-import android.net.Uri;
 public class MainActivity extends Activity 
 {
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {   //文字表示
+    protected void onCreate(Bundle savedInstanceState) {
+    	// 文字を表示
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-		//dcha 3 変更
+		// dcha_stateを３に変更
 		System.putInt(getContentResolver(), "dcha_state", 3);
+		// ナビバーを表示
 		System.putInt(getContentResolver(), "hide_navigation_bar", 0);
-		//開発者向けオプションを開く
-		Intent intent = new Intent(android.provider.Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS);
-		startActivity(intent);
 		
-		//3秒後にdcha 0に変更
+		// 3秒後に設定アプリを開く
 		new Handler().postDelayed(new Runnable() {
 				@Override
 				public void run() {
-					//dcha 0 変更
-					System.putInt(getContentResolver(), "dcha_state", 0);
-					
+					Intent o = new Intent(Settings.ACTION_SETTINGS);
+					startActivity(o);
 				}
 			}, 3000);
 		

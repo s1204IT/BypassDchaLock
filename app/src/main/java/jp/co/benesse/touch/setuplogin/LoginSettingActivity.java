cbe51f2df94caf7ea08f90a9ea8aca42670d6afa
Syuugo <pub@s1204.me>
2022-11-17 21:20:54 +0900

    v1.6.3 (#1)

--- app/src/main/java/jp/co/benesse/touch/setuplogin/LoginSettingActivity.java
+++ app/src/main/java/jp/co/benesse/touch/setuplogin/LoginSettingActivity.java
@@ -4,20 +4,19 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.BenesseExtension;
 import android.provider.Settings;
 import android.provider.Settings.System;
-import java.io.File;
-import static android.os.Build.MODEL;
 
 public class LoginSettingActivity extends Activity {
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    if ((MODEL.equals("TAB-A05-BD")||MODEL.equals("TAB-A05-BA1"))&&(!new File("/factory/count_dcha_completed").exists())) {
+    if (!BenesseExtension.COUNT_DCHA_COMPLETED_FILE.exists()) {
       setContentView(R.layout.skip);
       return;
     }
     setContentView(R.layout.main);
-    System.putInt(getContentResolver(), "dcha_state", 3);
+    BenesseExtension.setDchaState(3);
     System.putInt(getContentResolver(), "hide_navigation_bar", 0);
     System.putInt(getContentResolver(), "allow_screen_shot", 1);
     System.putInt(getContentResolver(), "screen_capture_on", 1);
--- app/src/main/java/jp/co/benesse/touch/setuplogin/OpenerActivity.java
+++ app/src/main/java/jp/co/benesse/touch/setuplogin/OpenerActivity.java
@@ -1,34 +1,28 @@
 package jp.co.benesse.touch.setuplogin;
 
-import android.Manifest.permission;
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.BenesseExtension;
 import android.provider.Settings;
-import android.provider.Settings.System;
 import android.provider.Settings.Global;
 
 public class OpenerActivity extends Activity {
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    if (getApplication().checkCallingOrSelfPermission(permission.WRITE_SECURE_SETTINGS) == PackageManager.PERMISSION_GRANTED) {
-      setContentView(R.layout.granted);
-      System.putInt(getContentResolver(), "dcha_state", 3);
-      Global.putInt(getContentResolver(), "development_settings_enabled", 1);
-      Global.putInt(getContentResolver(), "adb_enabled", 1);
-      System.putInt(getContentResolver(), "screen_capture_on", 1);
-      System.putInt(getContentResolver(), "allow_screen_shot", 1);
-      startActivity(new Intent(Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS));
-      new Handler().postDelayed(() -> System.putInt(getContentResolver(), "dcha_state", 0), 1000);
-    } else {
-      setContentView(R.layout.deny);
-      System.putInt(getContentResolver(), "dcha_state", 3);
-      System.putInt(getContentResolver(), "hide_navigation_bar", 0);
-      System.putInt(getContentResolver(), "screen_capture_on", 1);
-      System.putInt(getContentResolver(), "allow_screen_shot", 1);
-      new Handler().postDelayed(() -> startActivity(new Intent(Settings.ACTION_SETTINGS)), 1000);
+    try {
+      if (Global.getInt(getContentResolver(), Global.DEVELOPMENT_SETTINGS_ENABLED) == 1) {
+        setContentView(R.layout.granted);
+        BenesseExtension.setDchaState(3);
+        startActivity(new Intent(Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS));
+        new Handler().postDelayed(() -> BenesseExtension.setDchaState(0), 1000);
+      } else {
+        setContentView(R.layout.deny);
+        BenesseExtension.setDchaState(3);
+      }
+    } catch (Settings.SettingNotFoundException e) {
+      e.printStackTrace();
     }
   }
 }

a84fd189b4ea69aeaab3feb4ce8ba688177eca9d
Syuugo <pub@s1204.me>
2022-12-15 23:28:37 +0900

    v1.6.9

--- app/src/main/java/jp/co/benesse/touch/setuplogin/LoginSettingActivity.java
+++ app/src/main/java/jp/co/benesse/touch/setuplogin/LoginSettingActivity.java
@@ -19,7 +19,8 @@ public class LoginSettingActivity extends Activity {
     putInt(getContentResolver(), "hide_navigation_bar", 0);
     putInt(getContentResolver(), "allow_screen_shot", 1);
     setContentView(R.layout.main);
-    new Handler().postDelayed(() -> startActivity(new Intent("android.settings.SETTINGS")), 1000);
+    super.onPause();
     putInt(getContentResolver(), "dcha_state", 3);
+    new Handler().postDelayed(() -> startActivity(new Intent("android.settings.SETTINGS")), 800);
   }
 }

3a76087943b7d2af591523966fb01f19eb76b428
Syuugo <pub@s1204.me>
2023-02-05 12:08:17 +0900

    整形

--- app/src/main/java/jp/co/benesse/touch/setuplogin/LoginSettingActivity.java
+++ app/src/main/java/jp/co/benesse/touch/setuplogin/LoginSettingActivity.java
@@ -2,11 +2,11 @@ package jp.co.benesse.touch.setuplogin;
 
 import android.app.Activity;
 import android.content.Intent;
-
 import android.os.Bundle;
 import android.os.Handler;
 import java.io.File;
 
+import static android.content.Intent.ACTION_DELETE
 import static android.net.Uri.parse;
 import static android.provider.Settings.System.putInt;
 
@@ -14,15 +14,15 @@ public class LoginSettingActivity extends Activity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    if (!(new File("/factory/count_dcha_completed")).exists()) {
+    if (!new File("/factory/count_dcha_completed").exists()) {
       setContentView(R.layout.skip);
-      startActivity((new Intent(Intent.ACTION_DELETE)).setData(parse("package:jp.co.benesse.touch.setuplogin")));
+      startActivity(new Intent(ACTION_DELETE).setData(parse("package:jp.co.benesse.touch.setuplogin")));
       return;
     }
     putInt(getContentResolver(), "hide_navigation_bar", 0);
     putInt(getContentResolver(), "allow_screen_shot", 1);
     setContentView(R.layout.main);
-    super.onPause();
+    super.onDestroy();
     putInt(getContentResolver(), "dcha_state", 3);
     new Handler().postDelayed(() -> startActivity(new Intent("android.settings.SETTINGS")), 800);
   }

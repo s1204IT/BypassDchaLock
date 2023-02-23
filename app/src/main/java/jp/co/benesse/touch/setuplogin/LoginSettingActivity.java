a4fd9975e2f4fb4740a7b6fe96fc66ede0163120
Syuugo <pub@s1204.me>
2023-02-23 09:46:35 +0900

    `return`を廃止し`else`に変更

--- app/src/main/java/jp/co/benesse/touch/setuplogin/LoginSettingActivity.java
+++ app/src/main/java/jp/co/benesse/touch/setuplogin/LoginSettingActivity.java
@@ -16,13 +16,13 @@ public class LoginSettingActivity extends Activity {
     if (!new File("/factory/count_dcha_completed").exists()) {
       setContentView(R.layout.skip);
       startActivity(new Intent("android.intent.action.DELETE").setData(parse("package:jp.co.benesse.touch.setuplogin")));
-      return;
+    } else {
+      putInt(getContentResolver(), "hide_navigation_bar", 0);
+      putInt(getContentResolver(), "allow_screen_shot", 1);
+      setContentView(R.layout.main);
+      super.onDestroy();
+      putInt(getContentResolver(), "dcha_state", 3);
+      new Handler().postDelayed(() -> startActivity(new Intent("android.settings.SETTINGS")), 800);
     }
-    putInt(getContentResolver(), "hide_navigation_bar", 0);
-    putInt(getContentResolver(), "allow_screen_shot", 1);
-    setContentView(R.layout.main);
-    super.onDestroy();
-    putInt(getContentResolver(), "dcha_state", 3);
-    new Handler().postDelayed(() -> startActivity(new Intent("android.settings.SETTINGS")), 800);
   }
 }
